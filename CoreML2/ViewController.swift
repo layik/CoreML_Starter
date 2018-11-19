139e056e5777d257dcfe33061ccf8e7dea72bf52
Layik Hama <layik.hama@gmail.com>
2018-11-19 08:54:35 +0000

    AgeNet and terrible UI

--- CoreML2/ViewController.swift
+++ CoreML2/ViewController.swift
@@ -7,14 +7,104 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var guessingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+}
 
-
+extension ViewController {
+    
+    func detectAge(image: CIImage) {
+        guessingLabel.text = "Detecting age…"
+        
+        // Load the ML model through its generated class
+        guard let model = try? VNCoreMLModel(for: AgeNet().model) else {
+            fatalError("can't load AgeNet model")
+        }
+        
+        // Create request for Vision Core ML model loaded
+        let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
+            }
+            
+            // Update the UI on main queue
+            DispatchQueue.main.async { [weak self] in
+                self?.guessingLabel.text = "I think your age is \(topResult.identifier) years!"
+            }
+        }
+        
+        // Run the Core ML AgeNet classifier on global dispatch queue
+        let handler = VNImageRequestHandler(ciImage: image)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    @IBAction func photosButtonTapped(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            let alert = UIAlertController(title: "No photos", message: "This device does not support photos.", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            let alert = UIAlertController(title: "No camera", message: "This device does not support camera.", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        picker.cameraCaptureMode = .photo
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Dismiss the picker controller
+        dismiss(animated: true)
+        
+        // guard unwrap the image picked
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("couldn't load image")
+        }
+        
+        // Set the picked image to the UIImageView - imageView
+        imageView.image = image
+        
+        // Convert UIImage to CIImage to pass to the image request handler
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("couldn't convert UIImage to CIImage")
+        }
+        
+        detectAge(image: ciImage)
+    }
+    
 }
 
